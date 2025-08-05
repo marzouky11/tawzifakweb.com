13d8aa2e3f9c737066c5f3d5d73e9ac74d9bf741
Younes Marzouk <tawzifakweb@gmail.com>
2025-08-05 21:56:30 +0000

    مرحباً، لاحظت أن جميع الإعلانات المنشورة في المنصة، بالإضافة إلى الصفحات

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ require('dotenv').config({ path: './.env.local' });
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
