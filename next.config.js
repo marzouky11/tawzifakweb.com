e4a1161ad0f953cc85613343acf26ba59e32b1e7
marzouky11 <tawzifakweb@gmail.com>
2025-08-25 19:34:52 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
-
+here
 require('dotenv').config({ path: './.env.local' });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  compress: true, // Enable Gzip compression
   env: {
     NEXT_PUBLIC_RECAPTCHA_SITE_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
   },
