bbd5945a9d6696ab5b2fd1db8985c6dde3bea759
Younes Marzouk <ynssmarzouk@gmail.com>
2025-09-07 23:04:44 +0000

    اجعل بطاقات مباريات العمومية فقط خط واحد عادي متل كما كان في قسم فرص اله

--- src/components/competition-card.tsx
+++ src/components/competition-card.tsx
@@ -54,7 +54,7 @@ export function CompetitionCard({ competition }: CompetitionCardProps) {
   
   return (
     <Card 
-        className="flex flex-col rounded-lg bg-card shadow-md h-full transition-all hover:shadow-xl w-full overflow-hidden border-4 border-double"
+        className="flex flex-col rounded-lg bg-card shadow-md h-full transition-all hover:shadow-xl w-full overflow-hidden border-t-4"
         style={{ borderColor: sectionColor }}
     >
        <CardHeader className="p-4">
